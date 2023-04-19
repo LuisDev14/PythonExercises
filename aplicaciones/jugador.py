6826900f3ef373c4ae6be714eb4677a492bcd922
Luis Garcia <gbluisman@gmail.com>
2023-04-19 02:22:47 -0600

    [add] tuplas exercise

---
+++ aplicaciones/jugador.py
@@ -0,0 +1,5 @@
+jLocal = str(input("Ingresa el nombre del partido local: "))
+jVisitante = str(input("Ingresa el nombre del partido visitante: "))
+resul = str(input("resultado: "))
+datos = (jLocal, jVisitante, resul)
+print(f"{datos[0]} vs {datos[1]} quedaron {datos[2]}")
